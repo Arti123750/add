da5b9a70f650dc0dac892f77a2075e1ce06381ab
Arti123750 <bombalearti@gmail.com>
2020-01-28 14:49:35 +0530

    add

---
+++ src/main/java/com/abc/main/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.abc.main;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Hello world");
+	}
+
+}
